1ea279313bb43079f1e3fda735b6bff9a93e95e7
shaikrasheed <shaikrasheed6341@gmail.com>
2025-04-08 00:21:00 +0530

    nav bar done

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,10 +1,19 @@
+import Link from 'next/link'
 import React from 'react'
+import { FaBug } from "react-icons/fa";
 
 const Navbar = () => {
+    const links = [
+        {label:"Dashboard", herf:'/dashboard'},
+        {label:"issues",herf:'/issue'}
+    ]
   return (
-    <nav>
-        <div>logo
-        </div>
+    <nav className='flex space-x-6 border-b  p-4 px-6 h-14  items-center'>
+        <Link href= "/"><FaBug /></Link>
+        <ul className='flex space-x-6'>
+            {links.map(link => <Link key={link.herf} className='text-zinc-400 hover:text-zinc-900 transition-colors' href={link.herf}>{link.label}</Link>)}
+        </ul>
+
     </nav>
   )
 }
